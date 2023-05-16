dbd0108140b3ca155a640643faf8659364c54052
ridhobaihaki <hanyatugas9@gmail.com>
2023-05-16 13:15:50 +0700

    Sebuah commit

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class BallController : MonoBehaviour
 {
-
+    [SerializeField] CinemachineFreeLook look;
+    private void Update() {
+        look.enabled = Input.GetMouseButton(0);
+    }
 }
