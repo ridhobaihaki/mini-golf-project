a094bdc8765a6bd45bc4d3cbd935327b9c62459f
ridhobaihaki <hanyatugas9@gmail.com>
2023-05-16 17:02:35 +0700

    Sebuah komit

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,8 +5,33 @@ using Cinemachine;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] CinemachineFreeLook look;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float force;
+    bool shoot;
     private void Update() {
-        look.enabled = Input.GetMouseButton(0);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            shoot = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        if(shoot)
+        {
+            shoot = false;
+            Vector3 direction = Camera.main.transform.forward;
+            direction.y = 0;
+            rb.AddForce(direction * force, ForceMode.Impulse);
+        }
+
+        if(rb.velocity.sqrMagnitude < 0.01f && rb.velocity.sqrMagnitude > 0)
+        {
+            rb.velocity= Vector3.zero;
+        }
+    }
+
+    public bool IsMove()
+    {
+        return rb.velocity != Vector3.zero;
     }
 }
---
+++ Assets/PlayManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    [SerializeField] BallController ballController;
+    [SerializeField] CameraController camController;
+    private void Update() {
+        var inputActive = Input.GetMouseButton(0) && ballController.IsMove() == false;
+        camController.SetInputActive(inputActive);
+    }
+}
