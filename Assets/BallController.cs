2bc8665b5a42a4d8cf9f79568e6c57cf73b6f4d9
ridhobaihaki <hanyatugas9@gmail.com>
2023-05-20 18:44:54 +0700

    Sebuat komit

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,34 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.EventSystems;
 
-public class BallController : MonoBehaviour
+public class BallController : MonoBehaviour , IPointerDownHandler
 {
     [SerializeField] Collider col;
     [SerializeField] Rigidbody rb;
     [SerializeField] float force;
+    [SerializeField] LineRenderer aimLine;
     bool shoot;
+    bool shootingMode;
+    float forceFactor;
+
+    public bool ShootingMode { get => shootingMode; }
     
     private void Update() 
     {
-        if(Input.GetMouseButtonDown(0))
+        if(shootingMode)
         {
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            
-            if(Physics.Raycast(ray, out var hitInfo) && hitInfo.collider == col)
+            if(Input.GetMouseButtonDown(0))
             {
-                    shoot = true;
+                aimLine.gameObject.SetActive(true);
+            }
+            else if(Input.GetMouseButton(0))
+            {
+                var mouseViewportPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                var ballViewportPos = Camera.main.WorldToViewportPoint(this.transform.position);
+                var ballScreenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+                var pointerDirection = ballViewportPos - mouseViewportPos;
+                pointerDirection.z = 0;
+                
+                // draw aim
+                var positions = new Vector3[]{ballScreenPos, Input.mousePosition};
+                aimLine.SetPositions(positions);
+                
+                // force factor
+                forceFactor = pointerDirection.magnitude * 2;
+                Debug.Log(forceFactor);
+
+                // force direction
+                Debug.Log(pointerDirection.normalized);
+            }
+            else if(Input.GetMouseButtonUp(0))
+            {
+                shoot = true;
+                shootingMode = false;
+                aimLine.gameObject.SetActive(false);
             }
         }
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate() 
+    {
         if(shoot)
         {
             shoot = false;
             Vector3 direction = Camera.main.transform.forward;
             direction.y = 0;
-            rb.AddForce(direction * force, ForceMode.Impulse);
+            rb.AddForce(direction * force * forceFactor, ForceMode.Impulse);
         }
 
         if(rb.velocity.sqrMagnitude < 0.01f && rb.velocity.sqrMagnitude > 0)
@@ -41,5 +71,10 @@ public class BallController : MonoBehaviour
     public bool IsMove()
     {
         return rb.velocity != Vector3.zero;
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        shootingMode = true;
     }
 }
--- Assets/PlayManager.cs
+++ Assets/PlayManager.cs
@@ -7,7 +7,10 @@ public class PlayManager : MonoBehaviour
     [SerializeField] BallController ballController;
     [SerializeField] CameraController camController;
     private void Update() {
-        var inputActive = Input.GetMouseButton(0) && ballController.IsMove() == false;
+        var inputActive = Input.GetMouseButton(0) 
+        && ballController.IsMove() == false 
+        && ballController.ShootingMode == false;
+        
         camController.SetInputActive(inputActive);
     }
 }
