450c72520d6b2cee2abc08d0f8f262a0cf7e9e11
ridhobaihaki <hanyatugas9@gmail.com>
2023-05-27 19:28:27 +0700

    Sebuah komit

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -35,14 +35,9 @@ public class BallController : MonoBehaviour , IPointerDownHandler
             {
                 var mouseViewportPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
                 var ballViewportPos = Camera.main.WorldToViewportPoint(this.transform.position);
-                var ballScreenPos = Camera.main.WorldToScreenPoint(this.transform.position);
                 var pointerDirection = ballViewportPos - mouseViewportPos;
                 pointerDirection.z = 0;
                 
-                // // draw aim
-                // aimLine.transform.position = ballScreenPos;
-                // var positions = new Vector3[]{ballScreenPos, Input.mousePosition};
-                // aimLine.SetPositions(positions);
 
                 // force direction
                 ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -57,7 +52,16 @@ public class BallController : MonoBehaviour , IPointerDownHandler
                 aimWorld.transform.position = this.transform.position;
                 aimWorld.forward = forceDirection;
                 aimWorld.localScale = new Vector3(1, 1, 0.5f + forceFactor);
-            
+                
+
+                var ballScreenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+                var mouseScreenPos = Input.mousePosition;
+                ballScreenPos.z = 1f;
+                mouseScreenPos.z = 1f;
+                var positions = new Vector3[]{
+                    Camera.main.ScreenToWorldPoint(ballScreenPos),
+                    Camera.main.ScreenToWorldPoint(mouseScreenPos)};
+                aimLine.SetPositions(positions);
             }
             else if(Input.GetMouseButtonUp(0))
             {
