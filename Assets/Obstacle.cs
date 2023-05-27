6d122ef18d5b1a767e92f2f6ebd528e06af3d993
ridhobaihaki <hanyatugas9@gmail.com>
2023-05-28 01:13:03 +0700

    Sebuah komit
    Kemajuan proyek dengan penambahan speeder, dimulainya yang mungkin dikatakan sebagai penggunaan DOTween, dsb.

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] List<Transform> positions;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
